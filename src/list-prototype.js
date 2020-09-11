52f76b535d42836af020ae4b1b06270ff7d4634f
Hazem Bittar <mr.hazem@hotmail.fr>
2020-09-11 14:35:58 +0200

    Update list-prototype.js

--- src/list-prototype.js
+++ src/list-prototype.js
@@ -24,3 +24,53 @@ export const listPrototype = {
     return buttonEl;
   }
 };
+document.addEventListener("DOMContentLoaded", function () {
+	const todo = Object.create(null);
+
+	todo.newTodo = function (evt) {
+		evt.preventDefault();
+		const newTodo = document.getElementById("todo-item").value;
+		const ul = document.getElementById("todo-view");
+		const li = document.createElement("li");
+		li.innerHTML = newTodo + '<span>x</span>';
+
+		//using tertnary operator to add items to top of list if other items already exist
+		(ul.childElementCount == 0) ? ul.appendChild(li) : ul.insertBefore(li, ul.firstChild);
+
+		document.getElementById('todo-item').	value = "";
+
+		const span = li.getElementsByTagName("span");
+
+		span[0].addEventListener("click", todo.deleteTodo, false);
+	}
+
+	let todoCache = [];
+
+	todo.deleteTodo = function () {
+		todoCache.push(this.parentNode); 
+
+		this.parentNode.parentNode.removeChild(this.parentNode); 
+
+		return todoCache;
+	}
+
+	todo.undoDelete = function () {
+		const ul = document.getElementById("todo-view");
+
+		if (todoCache.length > 0 ) {
+			var lastTodo = todoCache.length - 1;
+
+			ul.appendChild(todoCache[lastTodo]);
+
+			todoCache.pop();
+		}
+	}
+
+	const todoSub = document.getElementById("submit-item");
+
+	todoSub.addEventListener("click", todo.newTodo, false);
+
+	const undo = document.getElementById('undo'); 
+
+	undo.addEventListener("click", todo.undoDelete, false);
+});
